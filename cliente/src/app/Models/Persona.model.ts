639648e4afe9ffb62ffeded3e99f7f81d0ed34b6
Alberto HP <a.herrera07@ufromail.cl>
2017-10-10 23:50:45 -0300

    Modificacion modelos de angular

--- cliente/src/app/Models/Persona.model.ts
+++ cliente/src/app/Models/Persona.model.ts
@@ -1,14 +1,13 @@
 export class Persona {
-  rut: string;
-  nombre1: string;
-  nombre2: string;
-  apellido1: string;
-  apellido2: string;
-  fono_casa: string;
-  fono_trabajo: string;
-  movil: string;
-  idGenero: number;
-  EstadoCivil_idEstado: number;
-  Comuna_idComuna: number;
-  Usuario_email: string;
+	rut: string;
+	nombre1: string;
+	nombre2: string;
+	apellido1: string;
+	apellido2: string;
+	fono_casa: string;
+	fono_trabajo: string;
+	movil: string;
+	idGenero: number;
+	EstadoCivil_idEstado: number;
+	Comuna_idComuna: number;
 }
--- cliente/src/app/Models/Usuario.model.ts
+++ cliente/src/app/Models/Usuario.model.ts
@@ -2,4 +2,5 @@ export class Usuario {
   email: string;
   password: string;
   Role_idRole: number;
+  Persona_rut: string;
 }
