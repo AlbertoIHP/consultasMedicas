75087c2ac83d5ea8801d46f45d7dac764e55f4ce
abobadilla02 <a.bobadilla02@ufromail.cl>
2017-11-06 20:24:35 -0300

    Inicio modulo citas, configuración de opciones a mostrar

--- cliente/src/app/Components/moduloCitas/homemc.component.ts
+++ cliente/src/app/Components/moduloCitas/homemc.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { EventosService } from '../../Services/eventos/eventos.service';
+
+import { Router } from '@angular/router';
+
+import { Usuario } from '../../Models/Usuario.model';
+import { UserService } from '../../Services/user/user.service';
+
+import { Role } from '../../Models/Role.model';
+import { RoleService } from '../../Services/role/role.service';
+
+import {UsuarioActual} from '../Globals/usuarioactual.component';
+
 
 @Component({
   selector: 'app-homemc',
@@ -6,10 +18,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homemc.component.css']
 })
 export class HomemcComponent implements OnInit {
+  public isLogeado = false
+   public rolUsuario:Role;
+   public usuario:Usuario;
+   public totalUsuarios: Usuario[];
+   public totalRoles: Role[];
+   public current:any;
+   public usuarioActual;
 
-  constructor() { }
+  constructor(public eventosService: EventosService, public router: Router,
+   public servicioRol:RoleService, public servicioUsuario: UserService) {
+    if( !(localStorage.getItem('currentUser')) )
+    {
+      this.router.navigate(['/'])
+    }else{
+      this.usuarioActual=new UsuarioActual(this.servicioRol,this.servicioUsuario);
+      this.rolUsuario=this.usuarioActual.rolUsuario;
+      this.usuario=this.usuarioActual.usuario;
+    }
+
+   }
 
   ngOnInit() {
   }
-
 }
