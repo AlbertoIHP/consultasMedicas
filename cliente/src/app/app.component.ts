4b19f9b8edb5f2f9960540f3f3be970069839d35
jp966 <j.tobias01@ufromail.cl>
2017-11-22 16:33:46 -0300

    Se integró fontawesome, evitando usar enlaces externos para el uso de iconos, y así los problemas asociados con la conexión a internet

--- cliente/src/app/app.component.ts
+++ cliente/src/app/app.component.ts
@@ -16,18 +16,6 @@ export class AppComponent {
 	constructor(public eventosService: EventosService,private router: Router,private iconRegistry: MatIconRegistry, sanitizer: DomSanitizer){
     //iconos de toolbar
     this.iconRegistry
-        .addSvgIcon('icono-paciente',
-            sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/patient.svg'));
-
-    this.iconRegistry
-        .addSvgIcon('icono-agenda',
-            sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/agenda.svg'));
-
-    this.iconRegistry
-        .addSvgIcon('icono-doctor',
-            sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/doctor.svg'));
-
-    this.iconRegistry
         .addSvgIcon('icono-logout',
             sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/exit.svg'));
 
@@ -35,37 +23,11 @@ export class AppComponent {
         .addSvgIcon('icono-editar-perfil',
             sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/tools.svg'));
 
-    //iconos para las acciones de las distintas secciones de módulos
-    this.iconRegistry
-        .addSvgIcon('icono-editar',
-            sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/repair.svg'));
-
-    this.iconRegistry
-        .addSvgIcon('icono-desactivar',
-            sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/cancel.svg'));
-
-    this.iconRegistry
-        .addSvgIcon('icono-ficha-medica',
-            sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/curriculum.svg'));
-
-    this.iconRegistry
-        .addSvgIcon('icono-prevision',
-            sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/commerce.svg'));
-
-    this.iconRegistry
-        .addSvgIcon('icono-agregar',
-            sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/add-user.svg'));
-
-    this.iconRegistry
-        .addSvgIcon('icono-eliminar',
-            sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/trash.svg'));
-
     this.iconRegistry
         .addSvgIcon('icono-error',
             sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/warning.svg'));
-    this.iconRegistry
-        .addSvgIcon('icono-alternativo',
-            sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/alternativo/pencil.svg'));
+
+  
 
     if(localStorage.getItem('currentUser'))
     {
