95bf66e50dc2ca5f1d182402bea03ba874516214
jp966 <j.tobias01@ufromail.cl>
2017-12-02 23:42:04 -0300

    Se desarrollaron los CRUDs para diagnóstico y via admin. medicamentos del módulo de antenciones

--- cliente/src/app/Components/moduloAtenciones/diagnostico/agregar-diagnostico/agregar-diagnostico.component.ts
+++ cliente/src/app/Components/moduloAtenciones/diagnostico/agregar-diagnostico/agregar-diagnostico.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { Diagnostico } from '../../../../Models/Diagnostico.model';
+import { DiagnosticoService } from '../../../../Services/diagnostico/diagnostico.service';
 
 @Component({
   selector: 'app-agregar-diagnostico',
   templateUrl: './agregar-diagnostico.component.html',
   styleUrls: ['./agregar-diagnostico.component.css']
 })
-export class AgregarDiagnosticoComponent implements OnInit {
+export class AgregarDiagnosticoComponent{
 
-  constructor() { }
+public nuevoDiagnostico:Diagnostico;
 
-  ngOnInit() {
+
+  constructor(
+  	public dialogRef: MatDialogRef<AgregarDiagnosticoComponent>,
+	public servicioDiagnostico:DiagnosticoService
+
+  	) {
+
+  		this.nuevoDiagnostico=new Diagnostico();
   }
+
+
+ agregarDiagnostico(){
+ 	this.servicioDiagnostico.registerDiagnostico(this.nuevoDiagnostico).subscribe(data=>{
+ 		this.dialogRef.close();
+ 	});
+ }
+
+ onNoClick()
+	{
+		this.dialogRef.close();
+	}
+
+
+ 
 
 }
